d1becd51ee8f422579512d836b6914d4bacc04da
franciscogafe <franciscogafe@gmail.com>
2022-08-25 17:01:48 -0500

    Modificando codigo con live boton encriptar

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const inputTexto = document.querySelector(".input-texto");
-const Mensaje = document.querySelector(".mensaje");
 
-function btnEncritar(){
-    const TextoEncritado = encriptar(inputTexto.vale);
-    Mensaje.value = TextoEncritado;
-    
+const inputTexto = document.querySelector(".input-texto");
+const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar(){
+    const TextoEncritado = encriptar(inputTexto.value);
+    mensaje.value = TextoEncritado;
+    mensaje.style.backgroundImage = "none";
+    inputTexto.value = "";
+
 }
 
 function encriptar(stringEncriptado){
     let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
     stringEncriptado = stringEncriptado.toLowerCase();
 
-    for(let i = 0; matrizCodigo.length; i++){
+    for(let i = 0; i < matrizCodigo.length; i++){
         if(stringEncriptado.includes[matrizCodigo[i][0]]){
             stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0]),(matrizCodigo[i][1]);
         }
     }
+    return stringEncriptado;
+
 
 }
